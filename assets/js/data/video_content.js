ca334946c51061a61be02e68a31aa63f5a183851
Fredo-Ronan <fredocode06@gmail.com>
2024-06-11 17:47:26 +0700

    add library and add video content feature

---
+++ assets/js/data/video_content.js
@@ -0,0 +1,9 @@
+/* 
+    File ini isinya adalah data list video yang ditampilkan di bagian Galeri pada website
+*/
+
+const video_contents = [
+    'https://cdn.pixabay.com/video/2021/09/21/89244-614062242_large.mp4',
+    'https://videos.pexels.com/video-files/856479/856479-hd_1366_720_25fps.mp4',
+    'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+]
--- assets/js/main_generate_html.js
+++ assets/js/main_generate_html.js
@@ -245,6 +245,81 @@ for(let i = 0; i < statistikDusunData.length; i++){
 // End Of Statistik Dusun Side
 
 
+// Video Content Side
+const videoContainer = document.getElementById("video-container");
+
+const sliderWrapper = document.createElement("div");
+sliderWrapper.className = "slider-wrapper";
+
+const ulSlidesContainer = document.createElement("ul");
+ulSlidesContainer.className = "slides-container";
+ulSlidesContainer.id = "slides-container";
+
+video_contents.forEach((data) => {
+    const liSlide = document.createElement("li");
+    liSlide.className = "slide";
+
+    const videoTag = document.createElement("video");
+    videoTag.className = "video-carousel";
+    videoTag.setAttribute("controls", "");
+
+    const sourceTag = document.createElement("source");
+    sourceTag.src = data;
+    sourceTag.type = "video/mp4";
+
+    videoTag.appendChild(sourceTag);
+    
+    liSlide.appendChild(videoTag);
+    ulSlidesContainer.appendChild(liSlide);
+});
+
+const divCarouselBtn = document.createElement("div");
+divCarouselBtn.className = "carousel-buttons";
+
+const buttonLeft = document.createElement("button");
+buttonLeft.className = "slide-arrow";
+buttonLeft.id = "slide-arrow-prev";
+buttonLeft.innerHTML = 
+`
+    <svg xmlns="http://www.w3.org/2000/svg" width="11" height="20" id="arrow"><path fill-rule="evenodd" d="M10.634.292a1.063 1.063 0 0 0-1.464 0L.607 8.556a1.95 1.95 0 0 0 0 2.827l8.625 8.325c.4.385 1.048.39 1.454.01a.975.975 0 0 0 .01-1.425l-7.893-7.617a.975.975 0 0 1 0-1.414l7.83-7.557a.974.974 0 0 0 0-1.413"></path></svg>
+`
+
+const buttonRight = document.createElement("button");
+buttonRight.className = "slide-arrow";
+buttonRight.id = "slide-arrow-next";
+buttonRight.innerHTML =
+`
+    <svg xmlns="http://www.w3.org/2000/svg" width="11" height="20" id="arrow"><path fill-rule="evenodd" d="M.366 19.708c.405.39 1.06.39 1.464 0l8.563-8.264a1.95 1.95 0 0 0 0-2.827L1.768.292A1.063 1.063 0 0 0 .314.282a.976.976 0 0 0-.011 1.425l7.894 7.617a.975.975 0 0 1 0 1.414L.366 18.295a.974.974 0 0 0 0 1.413"></path></svg>
+`
+
+divCarouselBtn.appendChild(buttonLeft);
+divCarouselBtn.appendChild(buttonRight);
+
+// append all to the slider wrapper
+sliderWrapper.appendChild(ulSlidesContainer);
+sliderWrapper.appendChild(divCarouselBtn);
+
+// append to main video container section
+videoContainer.appendChild(sliderWrapper);
+
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
+});
+
+// End Of Video Content Side
+
+
 // Pengurus Dusun Side
 const pengurusDusunContainer = document.getElementById("pengurus-dusun");
 
