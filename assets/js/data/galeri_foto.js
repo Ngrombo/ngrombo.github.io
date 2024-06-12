eeeebf588c7dc2cd4fdd3affd9a1b7343f1b2f70
Fredo-Ronan <fredocode06@gmail.com>
2024-06-12 16:06:49 +0700

    add galeri foto javascript mechanism

---
+++ assets/js/data/galeri_foto.js
@@ -0,0 +1,38 @@
+/*
+    File ini isinya adalah data foto - foto kegiatan yang biasa di lakukan di dusun yang nanti di tampilkan di bagian Galeri Foto
+    pada website
+*/
+
+const galeri_foto = [
+    {
+        gambar: "portfolio-1.jpg",
+        judul: "Foto 1",
+        link_ke_sumber_lain: ""
+    },
+    {
+        gambar: "portfolio-2.jpg",
+        judul: "Foto 2",
+        link_ke_sumber_lain: ""
+    },
+    {
+        gambar: "portfolio-3.jpg",
+        judul: "Foto 3",
+        link_ke_sumber_lain: ""
+    },
+    {
+        gambar: "portfolio-4.jpg",
+        judul: "Foto 4",
+        link_ke_sumber_lain: ""
+    },
+    {
+        gambar: "portfolio-5.jpg",
+        judul: "Foto 5",
+        link_ke_sumber_lain: ""
+    },
+    {
+        gambar: "portfolio-6.jpg",
+        judul: "Foto 6",
+        link_ke_sumber_lain: ""
+    },
+]
+
