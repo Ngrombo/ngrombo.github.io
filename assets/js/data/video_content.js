ac84ecd07a5b8036579b3757412be443bc9aa51b
Fredo-Ronan <fredocode06@gmail.com>
2024-07-31 06:42:32 +0700

    some changes

--- assets/js/data/video_content.js
+++ assets/js/data/video_content.js
@@ -4,8 +4,8 @@
 
 const video_contents = [
     {
-        videoLink: 'https://drive.google.com/file/d/13J_CJzsiw8XNcwAyTteIkNcPbimAjHwC/view?usp=sharing',
-        judul: "Video ",
-        deskripsi: "Deskripsi video 1"
+        videoLink: 'https://drive.google.com/file/d/1KY-9rZAenqHz-L4b6eN2inktpgv0MSTs/view?usp=drive_link',
+        judul: "Pemancingan Godokan",
+        deskripsi: "Spot Pemancingan Pemancingan Godokan"
     },
 ]
